93c1e0e3244beda3e9deb5d19a2e5715a367dd52
Aayushma Dahal <aayu.dahal100@gmail.com>
2024-02-27 00:04:59 +0900

    add state

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,7 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
      // Validate form fields using Zod
      const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
